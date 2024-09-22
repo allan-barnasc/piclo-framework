a4b025f3aeb10846ba38110d8db14bab6334bcff
Allan Nascimento <allan.nasc8@gmail.com>
2024-09-22 18:52:10 -0300

    feat(public/*): Design implementation

--- views/index.piclate.php
+++ views/index.piclate.php
@@ -1,4 +1,15 @@
 ?template("templates/main")
 ?inside("content")
-    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit debitis ipsum minima sint explicabo quidem id eaque ipsa, facere nobis, maiores architecto temporibus commodi magni impedit accusantium necessitatibus in doloribus!</h1>
+    <div class="bg-success vh-100 w-100 d-flex align-items-center justify-content-center">
+        <div class="w-50 rounded-3 bg-white border border-3 border-light shadow-lg d-flex flex-column align-items-center">
+            <div class="d-flex flex-row align-items-center gap-2 mt-4 mb-4">
+                <img src="images/piclo-logo.png" class="img-fluid" width="75" />
+                <h1 class="display-4 m-0">Piclo Framework</h1>
+            </div>
+            <p class="fs-5 mb-5">A simple <strong>PHP Framework</strong> for <strong>simple projects</strong></p>
+            <div class="w-100 p-3 bg-warning rounded-bottom-3 text-center">
+                <h1 class="display-3 text-white fw-bold">Under Construction</h1>
+            </div>
+        </div>
+    </div>
 ?endinside
--- views/templates/main.piclate.php
+++ views/templates/main.piclate.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/app.css" rel="stylesheet" />
+
     <title>Piclo Framework</title>
 </head>
 <body>
